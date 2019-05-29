07c28485b42eaae5b15c26f8b427593453984c01
aoihikaru <aoi.hikaru13@gmail.com>
2019-05-29 18:00:38 +0300

    создана главная страница
    и добавлена ссылка на одну из работ

---
+++ sedona/js/script-min.js
@@ -0,0 +1,1 @@
+﻿var btn=document.querySelector(".offer-form-title");btn.addEventListener("click",function(){document.querySelector(".offer-form-body").classList.toggle("offer-active")});
---
+++ sedona/js/script.js
@@ -0,0 +1,5 @@
+﻿var btn = document.querySelector('.offer-form-title');
+
+btn.addEventListener('click', function () {
+  document.querySelector('.offer-form-body').classList.toggle('offer-active');
+});
